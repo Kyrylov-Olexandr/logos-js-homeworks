98f4f7856a6acc2f8d3e83a42d4954df23344751
Kyrylov-Olexandr <59021146+Kyrylov-Olexandr@users.noreply.github.com>
2021-02-02 00:41:29 +0300

    Added try-catch block
    Added throwing exception and try-catch block

--- hw2/task3.js
+++ hw2/task3.js
@@ -1,16 +1,20 @@
 function calc(a, operation, b) {
     var result;
-    if (operation === "+") {
-        result = a + b;
-    } else if (operation === "-") {
-        result = a - b;
-    } else if (operation === "*") {
-        result = a * b;
-    } else if (operation === "/") {
-        result = a / b;
-    } else {
-        result = "Wrong parameter. (Permissible '+', '-', '*', '/')";
-    }
-    return result;
-}
-console.log(calc(4, "+", 2));
+    if (["+", "-", "*", "/"].includes(operation)) {
+        if (operation === "+") {
+            result = a + b;
+        } else if (operation === "-") {
+            result = a - b;
+        } else if (operation === "*") {
+            result = a * b;
+        } else if (operation === "/") {
+            result = a / b;
+        }
+        return result;
+    } else throw "Wrong parameter exception. (Permissible '+', '-', '*', '/')";
+};
+try {
+    console.log(calc(4, "2", 2));
+} catch (error) {
+    console.error(error);
+};
